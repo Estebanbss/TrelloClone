0d1c33c0ded6b42ef0e31857f339a17fa3ab70dc
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-02-23 20:33:44 -0500

    Update binary files and add new files

--- obj/Debug/net8.0/TrelloClone.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrelloClone.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrelloClone")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f4f396aa8e5adee01c0802e4c15cb398b8af99")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrelloClone")]
